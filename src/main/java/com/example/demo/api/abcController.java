c72a639d7235f804593d156cb85134502870d436
LimChangeun <cksrms1030@naver.com>
2025-03-18 12:02:44 +0900

    12

--- src/main/java/com/example/demo/api/abcController.java
+++ src/main/java/com/example/demo/api/abcController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class abcController {
     @GetMapping("/test")
     public String test() {
-        return "tset^^ :)";
+        return "tset12^^ :)";
     }
 }
